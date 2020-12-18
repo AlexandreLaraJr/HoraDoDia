c874c66d781fea13a18be951ad5615a88cbbe07e
Alexandre de Lara Castro Junior <68569165+AlexandreLaraJr@users.noreply.github.com>
2020-12-18 01:28:41 -0300

    arquivos adicionados

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregar(){
+    var msg = document.querySelector('div#msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    
+    msg.innerHTML = `Agora são ${hora}:${min}.`
+    
+    if(hora>= 5 && hora < 12){
+        //bom dia caraio
+        img.src = 'manha.png'
+        document.body.style.background = '#D98C4A'
+    }else if(hora > 12 && hora < 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#E5CDCD'
+    }else{
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#0F2426'
+    }
+}
